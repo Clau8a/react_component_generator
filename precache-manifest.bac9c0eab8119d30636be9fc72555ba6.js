83a2055eb3222c8528eca8fb9ca9a86aa4d425df
Claudia Andrade <claudia.andrade@wizeline.com>
2022-12-20 15:38:49 -0700

    Updates

---
+++ precache-manifest.bac9c0eab8119d30636be9fc72555ba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb5716e88d9a5cab74976f911103794",
+    "url": "/react_component_generator/index.html"
+  },
+  {
+    "revision": "6908e34db409fbdeeea4",
+    "url": "/react_component_generator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2755df041275caeb0a5d",
+    "url": "/react_component_generator/static/css/main.64459586.chunk.css"
+  },
+  {
+    "revision": "6908e34db409fbdeeea4",
+    "url": "/react_component_generator/static/js/2.79649226.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_component_generator/static/js/2.79649226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2755df041275caeb0a5d",
+    "url": "/react_component_generator/static/js/main.feb70a6f.chunk.js"
+  },
+  {
+    "revision": "a4a7aec58b662f80f14b",
+    "url": "/react_component_generator/static/js/runtime-main.bfca3020.js"
+  }
+]);
